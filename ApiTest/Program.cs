73827d975c70d5a88bfd215dbf06c97623c47dd3
Galimzyanov Dmitry <dmt021@gmail.com>
2012-08-05 22:09:35 +0600

    Fix

---
+++ ApiTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BtcE;
+
+namespace ApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ticker = BtceApi.GetTicker(BtcePair.BtcUsd);
+            var trades = BtceApi.GetTrades(BtcePair.BtcUsd);
+            var btcusdDepth = BtceApi.GetDepth(BtcePair.BtcUsd);
+
+            var btceApi = new BtceApi("YOUR-API-KEY", "your_secret_key");
+            var info = btceApi.GetInfo();
+            var transHistory = btceApi.GetTransHistory();
+            var tradeHistory = btceApi.GetTradeHistory(count: 20);
+            var orderList = btceApi.GetOrderList();
+            var tradeAnswer = btceApi.Trade(BtcePair.BtcUsd, TradeType.Sell, 20, 0.1m);
+            var cancelAnswer = btceApi.CancelOrder(tradeAnswer.OrderId);
+        }
+    }
+}
