7b7baa12c385387f51cd5505c96ec65994374a71
GnosisSuperFund <daniel@gnosisuper.com.au>
2013-11-12 22:25:48 +1030

    Split TradeInfoTypeHelper class into own file

--- BtceApi/TradeInfoType.cs
+++ BtceApi/TradeInfoType.cs
@@ -6,19 +6,5 @@ namespace BtcE
         Ask,
         Bid
     }
-    public class TradeInfoTypeHelper
-    {
-        public static TradeInfoType FromString(string s)
-        {
-            switch (s)
-            {
-                case "ask":
-                    return TradeInfoType.Ask;
-                case "bid":
-                    return TradeInfoType.Bid;
-                default:
-                    throw new ArgumentException();
-            }
-        }
-    }
+    
 }
---
+++ BtceApi/TradeInfoTypeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BtcE
+{
+    public class TradeInfoTypeHelper
+    {
+        public static TradeInfoType FromString(string s)
+        {
+            switch (s)
+            {
+                case "ask":
+                    return TradeInfoType.Ask;
+                case "bid":
+                    return TradeInfoType.Bid;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
