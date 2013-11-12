3945988c876afd510822831cb97d14fe1561f493
GnosisSuperFund <daniel@gnosisuper.com.au>
2013-11-12 21:18:18 +1030

    Implimented IDisposable

--- BtceApi/BtceApi.cs
+++ BtceApi/BtceApi.cs
@@ -17,7 +17,7 @@ using BtcE.Utils;
 using Newtonsoft.Json.Linq;
 namespace BtcE
 {
-	public class BtceApi
+	public class BtceApi : IDisposable
 	{
 		string key;
 		HMACSHA512 hashMaker;
@@ -232,5 +232,48 @@ namespace BtcE
 			if (request == null) throw new Exception("Non HTTP WebRequest");
 			return new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
 		}
+
+
+        /// <summary>
+        /// Clean up
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // null check
+                if (hashMaker != null)
+                {
+                    // invoke disposing
+                    hashMaker.Dispose();
+                    hashMaker = null; // null just to br safe
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clean up
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Clean up
+        /// </summary>
+        ~BtceApi()
+        {
+            Dispose(false);
+        }
+
+        public BtceApi(string key, string secret)
+        {
+            this.key = key;
+            hashMaker = new HMACSHA512(Encoding.ASCII.GetBytes(secret));
+            nonce = UnixTime.Now;
+        }
 	}
 }
