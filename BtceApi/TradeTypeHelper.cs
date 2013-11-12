144c58eb14cb54c2c06481335a20786d0ef03a03
GnosisSuperFund <daniel@gnosisuper.com.au>
2013-11-12 22:24:54 +1030

    Split TradeTypeHelper class into own file

---
+++ BtceApi/TradeTypeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BtcE
+{
+    public static class TradeTypeHelper
+    {
+        public static TradeType FromString(string s)
+        {
+            s = s.ToLower();
+            switch (s)
+            {
+                case "sell":
+                    return TradeType.Sell;
+                case "buy":
+                    return TradeType.Buy;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+        public static string ToString(TradeType v)
+        {
+            return Enum.GetName(typeof(TradeType), v).ToLowerInvariant();
+        }
+    }
+}
