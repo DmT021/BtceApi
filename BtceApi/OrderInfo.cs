da72e92ea8f9b74fab420494d3dcf1a9b8dbab18
GnosisSuperFund <daniel@gnosisuper.com.au>
2013-11-12 22:21:24 +1030

    Split OrderInfo class into own file

---
+++ BtceApi/OrderInfo.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BtcE
+{
+    public class OrderInfo
+    {
+        public decimal Price { get; private set; }
+        public decimal Amount { get; private set; }
+        public static OrderInfo ReadFromJObject(JArray o)
+        {
+            if (o == null)
+                return null;
+            return new OrderInfo()
+            {
+                Price = o.Value<decimal>(0),
+                Amount = o.Value<decimal>(1),
+            };
+        }
+    }
+}
