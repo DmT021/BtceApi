afa0b75729a1b035c3bba0a5b1f5d93cc7eafec6
Infinum <n074v41l4bl34u@gmail.com>
2013-12-07 23:47:16 +0100

    - removed second BtceApi constructor causing NullRerefenceException with this.instanseExchangeHost

--- BtceApi/BtceApi.cs
+++ BtceApi/BtceApi.cs
@@ -20,18 +20,81 @@ namespace BtcE
 {
 	public class BtceApi : IDisposable
 	{
-		private string key;
-		private HMACSHA512 hashMaker;
-		private UInt32 nonce;
-		private readonly string instanseExchangeHost;
 		public static string ExchangeHost = "https://btc-e.com/";
-
+		private readonly string instanseExchangeHost;
+		private bool disposed = false;
+		private object disposeLock = new object();
+		private HMACSHA512 hashMaker;
+		private string key;
+		private UInt32 nonce;
 		public BtceApi(string key, string secret, string exchangeHost = null)
 		{
 			this.key = key;
 			hashMaker = new HMACSHA512(Encoding.ASCII.GetBytes(secret));
 			nonce = UnixTime.Now;
 			this.instanseExchangeHost = exchangeHost ?? ExchangeHost;
+		}
+
+		/// <summary>
+		/// Clean up
+		/// </summary>
+		~BtceApi()
+		{
+			Dispose(false);
+		}
+
+		public static Depth GetDepth(BtcePair pair)
+		{
+			return Depth.ReadFromJObject(JObject.Parse(Query(string.Format("{1}api/2/{0}/depth", BtcePairHelper.ToString(pair), ExchangeHost))));
+		}
+
+		public static decimal GetFee(BtcePair pair)
+		{
+			return JObject.Parse(Query(string.Format("{1}api/2/{0}/fee", BtcePairHelper.ToString(pair), ExchangeHost))).Value<decimal>("trade");
+		}
+
+		public static Ticker GetTicker(BtcePair pair)
+		{
+			return Ticker.ReadFromJObject(JObject.Parse(Query(string.Format("{1}api/2/{0}/ticker", BtcePairHelper.ToString(pair), ExchangeHost)))["ticker"] as JObject);
+		}
+
+		public static TradeInfo[] GetTrades(BtcePair pair)
+		{
+			return JArray.Parse(Query(string.Format("{1}api/2/{0}/trades", BtcePairHelper.ToString(pair), ExchangeHost))).OfType<JObject>().Select(TradeInfo.ReadFromJObject).ToArray();
+		}
+
+		public CancelOrderAnswer CancelOrder(int orderId)
+		{
+			var args = new Dictionary<string, string>()
+						{
+								{ "method", "CancelOrder" },
+								{ "order_id", orderId.ToString() }
+						};
+			var result = JObject.Parse(Query(args));
+			if (result.Value<int>("success") == 0) throw new BtceException(result.Value<string>("error"));
+			return CancelOrderAnswer.ReadFromJObject(result["return"] as JObject);
+		}
+
+		/// <summary>
+		/// Clean up
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		public OrderList GetActiveOrders(BtcePair? pair = null)
+		{
+			var args = new Dictionary<string, string>()
+						{
+								{ "method", "ActiveOrders" }
+						};
+			if (pair != null) args.Add("pair", BtcePairHelper.ToString(pair.Value));
+			var result = JObject.Parse(Query(args));
+			if (result.Value<int>("success") == 0)
+				throw new BtceException(result.Value<string>("error"));
+			return OrderList.ReadFromJObject(result["return"] as JObject);
 		}
 
 		public UserInfo GetInfo()
@@ -41,56 +104,6 @@ namespace BtcE
 			if (result.Value<int>("success") == 0)
 				throw new BtceException(result.Value<string>("error"));
 			return UserInfo.ReadFromJObject(result["return"] as JObject);
-		}
-
-		public TransHistory GetTransHistory(
-			int? from = null,
-			int? count = null,
-			int? fromId = null,
-			int? endId = null,
-			bool? orderAsc = null,
-			DateTime? since = null,
-			DateTime? end = null
-			)
-		{
-			var args = new Dictionary<string, string>() { { "method", "TransHistory" } };
-
-			if (from != null) args.Add("from", from.Value.ToString());
-			if (count != null) args.Add("count", count.Value.ToString());
-			if (fromId != null) args.Add("from_id", fromId.Value.ToString());
-			if (endId != null) args.Add("end_id", endId.Value.ToString());
-			if (orderAsc != null) args.Add("order", orderAsc.Value ? "ASC" : "DESC");
-			if (since != null) args.Add("since", UnixTime.GetFromDateTime(since.Value).ToString());
-			if (end != null) args.Add("end", UnixTime.GetFromDateTime(end.Value).ToString());
-			var result = JObject.Parse(Query(args));
-			if (result.Value<int>("success") == 0) throw new Exception(result.Value<string>("error"));
-			return TransHistory.ReadFromJObject(result["return"] as JObject);
-		}
-
-		public TradeHistory GetTradeHistory(
-			int? from = null,
-			int? count = null,
-			int? fromId = null,
-			int? endId = null,
-			bool? orderAsc = null,
-			DateTime? since = null,
-			DateTime? end = null
-			)
-		{
-			var args = new Dictionary<string, string>() { { "method", "TradeHistory" } };
-
-			if (from != null) args.Add("from", from.Value.ToString());
-			if (count != null) args.Add("count", count.Value.ToString());
-			if (fromId != null) args.Add("from_id", fromId.Value.ToString());
-			if (endId != null) args.Add("end_id", endId.Value.ToString());
-			if (orderAsc != null) args.Add("order", orderAsc.Value ? "ASC" : "DESC");
-			if (since != null) args.Add("since", UnixTime.GetFromDateTime(since.Value).ToString());
-			if (end != null) args.Add("end", UnixTime.GetFromDateTime(end.Value).ToString());
-
-			var result = JObject.Parse(Query(args));
-			if (result.Value<int>("success") == 0)
-				throw new BtceException(result.Value<string>("error"));
-			return TradeHistory.ReadFromJObject(result["return"] as JObject);
 		}
 
 		/// <summary>
@@ -123,17 +136,54 @@ namespace BtcE
 			throw new BtceException("GetOrderList() is depricated and will no longer be supported as of Nov. 1st 2013. Please use GetActiveOrders() instead.");
 		}
 
-		public OrderList GetActiveOrders(BtcePair? pair = null)
+		public TradeHistory GetTradeHistory(
+			int? from = null,
+			int? count = null,
+			int? fromId = null,
+			int? endId = null,
+			bool? orderAsc = null,
+			DateTime? since = null,
+			DateTime? end = null
+			)
 		{
-			var args = new Dictionary<string, string>()
-						{
-								{ "method", "ActiveOrders" }
-						};
-			if (pair != null) args.Add("pair", BtcePairHelper.ToString(pair.Value));
+			var args = new Dictionary<string, string>() { { "method", "TradeHistory" } };
+
+			if (from != null) args.Add("from", from.Value.ToString());
+			if (count != null) args.Add("count", count.Value.ToString());
+			if (fromId != null) args.Add("from_id", fromId.Value.ToString());
+			if (endId != null) args.Add("end_id", endId.Value.ToString());
+			if (orderAsc != null) args.Add("order", orderAsc.Value ? "ASC" : "DESC");
+			if (since != null) args.Add("since", UnixTime.GetFromDateTime(since.Value).ToString());
+			if (end != null) args.Add("end", UnixTime.GetFromDateTime(end.Value).ToString());
+
 			var result = JObject.Parse(Query(args));
 			if (result.Value<int>("success") == 0)
 				throw new BtceException(result.Value<string>("error"));
-			return OrderList.ReadFromJObject(result["return"] as JObject);
+			return TradeHistory.ReadFromJObject(result["return"] as JObject);
+		}
+
+		public TransHistory GetTransHistory(
+			int? from = null,
+			int? count = null,
+			int? fromId = null,
+			int? endId = null,
+			bool? orderAsc = null,
+			DateTime? since = null,
+			DateTime? end = null
+			)
+		{
+			var args = new Dictionary<string, string>() { { "method", "TransHistory" } };
+
+			if (from != null) args.Add("from", from.Value.ToString());
+			if (count != null) args.Add("count", count.Value.ToString());
+			if (fromId != null) args.Add("from_id", fromId.Value.ToString());
+			if (endId != null) args.Add("end_id", endId.Value.ToString());
+			if (orderAsc != null) args.Add("order", orderAsc.Value ? "ASC" : "DESC");
+			if (since != null) args.Add("since", UnixTime.GetFromDateTime(since.Value).ToString());
+			if (end != null) args.Add("end", UnixTime.GetFromDateTime(end.Value).ToString());
+			var result = JObject.Parse(Query(args));
+			if (result.Value<int>("success") == 0) throw new Exception(result.Value<string>("error"));
+			return TransHistory.ReadFromJObject(result["return"] as JObject);
 		}
 
 		public TradeAnswer Trade(BtcePair pair, TradeType type, decimal rate, decimal amount)
@@ -151,16 +201,66 @@ namespace BtcE
 			return TradeAnswer.ReadFromJObject(result["return"] as JObject);
 		}
 
-		public CancelOrderAnswer CancelOrder(int orderId)
+		/// <summary>
+		/// Clean up
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected virtual void Dispose(bool disposing)
 		{
-			var args = new Dictionary<string, string>()
+			// make thread safe
+			lock (disposeLock)
+			{
+				// conform to reference example: http://msdn.microsoft.com/en-us/library/fs2xkftw.aspx
+				if (!disposed)
+				{
+					if (disposing)
+					{
+						// null check
+						if (hashMaker != null)
 						{
-								{ "method", "CancelOrder" },
-								{ "order_id", orderId.ToString() }
-						};
-			var result = JObject.Parse(Query(args));
-			if (result.Value<int>("success") == 0) throw new BtceException(result.Value<string>("error"));
-			return CancelOrderAnswer.ReadFromJObject(result["return"] as JObject);
+							// invoke disposing
+							hashMaker.Dispose();
+							hashMaker = null; // null just to br safe
+						}
+					}
+				}
+			}
+		}
+
+		private static string BuildPostData(Dictionary<string, string> d)
+		{
+			StringBuilder s = new StringBuilder();
+			foreach (var item in d)
+			{
+				s.AppendFormat("{0}={1}", item.Key, HttpUtility.UrlEncode(item.Value));
+				s.Append("&");
+			}
+			if (s.Length > 0) s.Remove(s.Length - 1, 1);
+			return s.ToString();
+		}
+
+		private static string ByteArrayToString(byte[] ba)
+		{
+			return BitConverter.ToString(ba).Replace("-", "");
+		}
+
+		private static string DecimalToString(decimal d)
+		{
+			return d.ToString(CultureInfo.InvariantCulture);
+		}
+
+		private static string Query(string url)
+		{
+			var request = WebRequest.Create(url);
+			request.Proxy = WebRequest.DefaultWebProxy;
+			request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+			if (request == null) throw new Exception("Non HTTP WebRequest");
+			return new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
+		}
+
+		private UInt32 GetNonce()
+		{
+			return nonce++;
 		}
 
 		private string Query(Dictionary<string, string> args)
@@ -186,104 +286,5 @@ namespace BtcE
 			reqStream.Close();
 			return new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
 		}
-
-		private static string ByteArrayToString(byte[] ba)
-		{
-			return BitConverter.ToString(ba).Replace("-", "");
-		}
-
-		private static string BuildPostData(Dictionary<string, string> d)
-		{
-			StringBuilder s = new StringBuilder();
-			foreach (var item in d)
-			{
-				s.AppendFormat("{0}={1}", item.Key, HttpUtility.UrlEncode(item.Value));
-				s.Append("&");
-			}
-			if (s.Length > 0) s.Remove(s.Length - 1, 1);
-			return s.ToString();
-		}
-
-		private UInt32 GetNonce()
-		{
-			return nonce++;
-		}
-
-		private static string DecimalToString(decimal d)
-		{
-			return d.ToString(CultureInfo.InvariantCulture);
-		}
-
-		public static Depth GetDepth(BtcePair pair)
-		{
-			return Depth.ReadFromJObject(JObject.Parse(Query(string.Format("{1}api/2/{0}/depth", BtcePairHelper.ToString(pair), ExchangeHost))));
-		}
-
-		public static Ticker GetTicker(BtcePair pair)
-		{
-			return Ticker.ReadFromJObject(JObject.Parse(Query(string.Format("{1}api/2/{0}/ticker", BtcePairHelper.ToString(pair), ExchangeHost)))["ticker"] as JObject);
-		}
-
-		public static TradeInfo[] GetTrades(BtcePair pair)
-		{
-			return JArray.Parse(Query(string.Format("{1}api/2/{0}/trades", BtcePairHelper.ToString(pair), ExchangeHost))).OfType<JObject>().Select(TradeInfo.ReadFromJObject).ToArray();
-		}
-
-		public static decimal GetFee(BtcePair pair)
-		{
-			return JObject.Parse(Query(string.Format("{1}api/2/{0}/fee", BtcePairHelper.ToString(pair), ExchangeHost))).Value<decimal>("trade");
-		}
-
-		private static string Query(string url)
-		{
-			var request = WebRequest.Create(url);
-			request.Proxy = WebRequest.DefaultWebProxy;
-			request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
-			if (request == null) throw new Exception("Non HTTP WebRequest");
-			return new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
-		}
-
-
-        /// <summary>
-        /// Clean up
-        /// </summary>
-        /// <param name="disposing"></param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                // null check
-                if (hashMaker != null)
-                {
-                    // invoke disposing
-                    hashMaker.Dispose();
-                    hashMaker = null; // null just to br safe
-                }
-            }
-        }
-
-        /// <summary>
-        /// Clean up
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Clean up
-        /// </summary>
-        ~BtceApi()
-        {
-            Dispose(false);
-        }
-
-        public BtceApi(string key, string secret)
-        {
-            this.key = key;
-            hashMaker = new HMACSHA512(Encoding.ASCII.GetBytes(secret));
-            nonce = UnixTime.Now;
-        }
 	}
 }
